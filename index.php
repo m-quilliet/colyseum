2bf77f255cb66dcce6afc4bf7a06d87140a3608e
maibelline quilliet <mayb80@hotmail.fr>
2022-04-07 14:04:15 +0200

    commit

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php 
-$dsn = 'mysql:dbname=colyseum;host=127.0.0.1';
+$dsn = 'mysql:dbname=colyseum;host=localhost';
 $user = 'root';
+$pass = 'root';
 
-$pdo = new PDO($dsn, $user);
+$pdo = new PDO($dsn, $user, $pass);
+
+$query= $pdo->query('SELECT * FROM clients');
 
 require_once(dirname(__FILE__).'/views/templates/header.php');
 require_once(dirname(__FILE__).'/views/templates/footer.php');
